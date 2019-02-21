bd8924996397392f0e6f3bdb251934e593f4456b
chek <israel.gavriel@gmail.com>
2019-02-21 22:47:44 +0700

    alignin file names, background

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './app.sass'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="overlay">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
