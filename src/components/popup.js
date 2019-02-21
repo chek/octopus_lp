3209e609312ea2361bbd66df64968eeb0fed0e2c
chek <israel.gavriel@gmail.com>
2019-02-22 00:04:50 +0700

    adding button

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import Button from '@material-ui/core/Button'
+//import Button, { ButtonProps } from '@material-ui/core/Button'
 
 class Popup extends Component {
   render() {
+    const inputProps = {
+      variant: "contained",
+      color: "primary"
+    }
     return (
+      <Dialog aria-labelledby="simple-dialog-title" open={true}>
+        <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+        <div style={{ display: 'inline-flex' }}>
+            <div>
+              <Button color="default" >
+                Default
+              </Button>
+            </div>
+            <div style={{ alignSelf: 'center' }}>
+              <Button {...inputProps} >
+                Default
+              </Button>
+            </div>
+        </div>
+      </Dialog>
+    )
+    /*
         <div role="dialog" className="MuiModal-root-15 MuiDialog-root-1" aria-labelledby="simple-dialog-title">
           <div className="MuiBackdrop-root-17" aria-hidden="true" >
           </div>
@@ -16,8 +39,7 @@ class Popup extends Component {
             </div>
           </div>
         </div>
-    )
-    /*
+
     <Dialog aria-labelledby="simple-dialog-title" open={true}>
       <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
     </Dialog>
